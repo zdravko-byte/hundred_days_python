f1f15e3a08a2199e9c127438f492b9f8bea40d51
zdravko-byte <zdravko.talevski@gmail.com>
2024-08-04 15:16:11 +0200

    after variables

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,1 +1,20 @@
-print ("Hello World!\nHello World!")
+# print (len("Hello World!\nHello World!"))
+# num1 = int(input())
+# num2 = int(input())
+#
+# print(num1 * num2)
+#
+# name = input("what is your name")
+
+# a = input("Type your name")
+# b = input("Type your surname")
+# c = a
+# d = b
+# a = d
+# b = c
+# print(a)
+# print(b)
+
+city_name = input("What's the name of the city you were born in?\n")
+pet_name = input("What was your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
