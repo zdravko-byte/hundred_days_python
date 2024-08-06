1f8ef0bec92c4ec81e12ead08ecc00788f50ce76
zdravko-byte <zdravko.talevski@gmail.com>
2024-08-06 23:54:33 +0200

    mac -> calc

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -1,16 +1,36 @@
-print("Hello"[4])
-
-print(len("ckjbwjkwcwjcnw"))
-
-print(type("zdravko"))
-print(type(True))
-print(type(234))
-print(type(200.78))
+# print("Hello"[4])
+#
+# print(len("ckjbwjkwcwjcnw"))
+#
+# print(type("zdravko"))
+# print(type(True))
+# print(type(234))
+# print(type(200.78))
 
 
 #print("Print the numbers of letters in your name " + len(input("What is your name\n"))
 
-name_of_the_user = input("What is your name\n")
-number_of_letters = len(name_of_the_user)
+# name_of_the_user = input("What is your name\n")
+# number_of_letters = len(name_of_the_user)
+#
+# print("Print the number of letters in your name: " + str(number_of_letters))
 
-print("Print the number of letters in your name: " + str(number_of_letters))
+# print(2**2)
+# print(2/2)
+# print(2//2)
+# print(2*2)
+# print(2+2)
+# a = 5
+# b = 7
+# print(a+b)
+# print(2-2)
+#
+# print(3*3+3/3-3)
+# print(3*(3+3)/3-3)
+
+bmi = 90/1.75**2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi, 2))
+
