25e0cc179603e9f9f2ffb1d5decd8e23a0d33ed4
zdravko-byte <zdravko.talevski@gmail.com>
2024-08-06 22:35:49 +0200

    mac -> meh task

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -3,4 +3,14 @@ print("Hello"[4])
 print(len("ckjbwjkwcwjcnw"))
 
 print(type("zdravko"))
+print(type(True))
+print(type(234))
+print(type(200.78))
 
+
+#print("Print the numbers of letters in your name " + len(input("What is your name\n"))
+
+name_of_the_user = input("What is your name\n")
+number_of_letters = len(name_of_the_user)
+
+print("Print the number of letters in your name: " + str(number_of_letters))
