35ff8d04aac80c4e8c8e22abb2c7aa16bb9bbb70
zdravko-byte <zdravko.talevski@gmail.com>
2024-08-11 12:25:56 +0200

    mac -> day 3

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,15 +1,41 @@
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height?\n"))
-# if height >= 120:
-#     print("Enjoy your ride")
-# else:
-#     print("You are too small. Sorry.")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height?\n"))
+if height >= 120:
+    print("Enjoy your ride")
+    age = int(input("What is your age "))
+    if age <= 12:
+        print("That would be $5")
+    elif age <= 18:
+        print("That would be $7")
+    else:
+        print("That would be $12")
+else:
+    print("You are too small. Sorry.")
 
 # a = 10 % 3
 # print(a)
 
-number = int(input("Please enter a number\n"))
-if number % 2 == 0:
-    print("The number you entered is an even number")
+# number = int(input("Please enter a number\n"))
+# if number % 2 == 0:
+#     print("The number you entered is an even number")
+# else:
+#     print("The number you entered is an odd number")
+
+
+# Enter your height in meters e.g., 1.55
+height = float(input())
+# Enter your weight in kilograms e.g., 72
+weight = int(input())
+
+bmi = weight / (height ** 2)
+#bmi = round(bmi, 5)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
 else:
-    print("The number you entered is an odd number")
+  print(f"Your BMI is {bmi}, you are clinically obese.")
--- overall.py
+++ overall.py
@@ -99,3 +99,20 @@ print(f"Each person should pay: ${split}")
 # else:
 #     print("The number you entered is an odd number")
 
+# Enter your height in meters e.g., 1.55
+height = float(input())
+# Enter your weight in kilograms e.g., 72
+weight = int(input())
+
+bmi = weight / (height ** 2)
+#bmi = round(bmi, 5)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
