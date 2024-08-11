08c2a0a0ab6a8c72ef5d7c33679b610517a91dec
zdravko-byte <zdravko.talevski@gmail.com>
2024-08-11 11:01:02 +0200

    mac -> day 3

---
+++ day_3/day3.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height?\n"))
+if height >= 120:
+    print("Enjoy your ride")
+else:
+    print("You are too small. Sorry.")
--- overall.py
+++ overall.py
@@ -83,6 +83,11 @@ print(f"Each person should pay: ${split}")
 
 
 #Day - 3 (Control Flow, Logical Operators)
-
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height?\n"))
+# if height >= 120:
+#     print("Enjoy your ride")
+# else:
+#     print("You are too small. Sorry.")
 
 
